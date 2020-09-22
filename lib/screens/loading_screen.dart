036fdb5cc4fe40fff55383fe910a071e2f55cc49
abdulaziz-damlahi <abood_d@live.com>
2020-09-22 19:48:55 +0300

    json format

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/location.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -25,11 +26,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get(
+    http.Response response = await http.get(
         'https://api.openweathermap.org/data/2.5/onecall?lat=36.7523705&lon=34.5496791&exclude=hourly&appid=1eb0ce3a477f044560005da255c71423');
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodedata = jsonDecode(data);
+
+      double temp = decodedata['current']['temp'];
+      String cond = decodedata['current']['weather'][0]['description'];
+      String city = decodedata['timezone'];
+
+      print(temp);
+      print(cond);
+      print(city);
     } else {
       print(response.statusCode);
     }
